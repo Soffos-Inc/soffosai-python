f831a8f46fd99a2247e52e9adc447b21c8a3d500
franco <franco.such@Soffos.ai>
2023-03-23 17:19:22 +0800

    Soffos SDK start

---
+++ src/soffos.py
@@ -0,0 +1,14 @@
+'''
+Copyright (c)2022 - Soffos.ai - All rights reserved
+Created at: 2023-03-23
+Purpose: The main module of Soffos
+-----------------------------------------------------
+'''
+
+import requests
+
+
+class Client:
+
+    def __init__(self) -> None:
+        pass
