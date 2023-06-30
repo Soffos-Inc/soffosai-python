6f6e2f0ce009a0d81fd02827594ed5d03e3c863f
franco <franco.such@Soffos.ai>
2023-06-30 12:44:13 +0800

    tests: subclass_pipeline

--- soffosai/core/__init__.py
+++ soffosai/core/__init__.py
@@ -1,3 +1,4 @@
 from .services import *
+from .services.service import inspect_arguments
 from .nodes import NodeConfig
 from .pipeline import Pipeline
--- tests/pipelines/subclass_pipeline.py
+++ tests/pipelines/subclass_pipeline.py
@@ -1,9 +1,8 @@
 '''
-Copyright (c)2022 - Soffos.ai - All rights reserved
-Created at: 2023-06-23
-Purpose: Define the standard Pipeline for converting then ingesting a file
------------------------------------------------------
+This is a better way to create your custom Pipeline.
+The __call__ method gives you the power to put the arguments and makes calling your Pipeline so much easier
 '''
+
 from soffosai import ServiceString
 from soffosai.core import NodeConfig, Pipeline, inspect_arguments
 
@@ -13,7 +12,11 @@ class FileIngestPipeline(Pipeline):
     A Soffos Pipeline that takes a file, convert it to its text content then saves it to Soffos db.
     the output is a list containing the output object of file converter and document ingest
     '''
+
+    # override the constructor of the Pipeline
     def __init__(self, **kwargs) -> None:
+
+        # define your nodes
         file_converter_node = NodeConfig(
             service=ServiceString.FILE_CONVERTER,
             source = {
@@ -30,11 +33,23 @@ class FileIngestPipeline(Pipeline):
             }
         )
 
+        # define the stages
         stages = [file_converter_node, document_ingest_node]
         use_defaults = False
+
+        # supply the stages to the superclass constructor
         super().__init__(stages=stages, use_defaults=use_defaults, **kwargs)
 
 
-    def __call__(self, user, file, name):
-        user_input = inspect_arguments(self.__call__, user, file, name)
+    # override the callable method
+    def __call__(self, user, file, name): # define what your pipeline needs, arguments instead of dictionary
+        user_input = inspect_arguments(self.__call__, user, file, name) # convert the args to dict
         return super().__call__(user_input)
+
+    '''
+    The inspect_arguments helper function takes the function name as the first argument then
+    the rest of the arguments of the same function. Please put them in order you assign them 
+    to the function itself. As you can observe, __call__ and inspect_arguments both have the 
+    arguments listed as user, file, name.
+    '''
+    
