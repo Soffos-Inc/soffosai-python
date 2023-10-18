04e892c587db552c03bf031d74bdaeb1a9ecd758
franco <franco.such@Soffos.ai>
2023-10-18 12:53:14 +0800

    version 0.1.2

--- soffosai/utils/check_input.py
+++
@@ -1,7 +0,0 @@
-'''
-Copyright (c)2022 - Soffos.ai - All rights reserved
-Created at: 2023-04-01
-Purpose: checks the datatype of a dictionary value recursively
------------------------------------------------------
-'''
-
